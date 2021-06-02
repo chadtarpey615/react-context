3e9b6cd6ff80da48898bd733e52b849f0638c5eb
Chad Tarpey <chadtarpey@gmail.com>
2021-06-02 16:33:10 -0500

    finsished setting up webpack and create todolist and form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { hot } from "react-hot-loader";
 import "./app.css";
 const App = () => {
     return (
         <div className="App">
-            <h1>Hello world</h1>
+            <h1>Hello world!!!</h1>
+            <h2>test1</h2>
+            <h3>test 4454</h3>
         </div>
     )
 }
 
-export default App
+export default hot(module)(App)
