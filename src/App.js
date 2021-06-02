e7a05ce218fe839bf746efb21bb3bde6051c23e9
Chad Tarpey <chadtarpey@gmail.com>
2021-06-01 20:49:50 -0500

    installing webpack

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./app.css";
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Hello world</h1>
+        </div>
+    )
+}
+
+export default App
